cfce4b1cc5fcda1d4303318ca38409cc8bf9cf29
khaledkzy <khaledkzy@gmail.com>
2019-10-13 08:32:37 +0100

    Add angular files uploader

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { HttpClient, HttpEventType, HttpHeaders } from '@angular/common/http';
+import { FileUploader } from 'ng2-file-upload';
+
+interface Book { id: number; name: string; content?: string }
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontendClone';
+  fileData: File = null;
+  constructor(private http: HttpClient, private changeDetection: ChangeDetectorRef) { }
+  fileUploaded = ''
+
+  @ViewChild('fileInput', { static: true }) fileInput: ElementRef;
+
+  uploader: FileUploader;
+  isDropOver: boolean;
+  allBooks: any = []
+  bookName = ''
+
+  ngOnInit(): void {
+
+    const headers = [{ name: 'Accept', value: 'application/json', crossDomain: true }];
+    this.uploader = new FileUploader({ url: 'http://localhost:9090/upload', autoUpload: true, headers });
+    this.uploader.onBeforeUploadItem = (item) => {
+      item.withCredentials = false;
+    };
+    this.uploader.onCompleteAll = () => {
+      console.log('File: ' + this.uploader.queue[0].file.name + ' - Uploaded Successfully')
+      this.fileUploaded = 'File: ' + this.uploader.queue[0].file.name + ' - Uploaded Successfully'
+      this.findall()
+    }
+  }
+
+  fileOverAnother(e: any): void {
+    this.isDropOver = e;
+  }
+
+  fileClicked() {
+    this.fileInput.nativeElement.click();
+  }
+
+
+  createBook() {
+    this.http.post('http://localhost:9090/book', { name: this.bookName }).subscribe((res) => {
+      // Call the get request to check if a book has been created.
+      this.findall()
+      console.log('Created a customer');
+      console.log(res);
+    });
+  }
+
+  deleteAll() {
+    this.http.get('http://localhost:9090/deleteAll').subscribe((res) => {
+      // Call the get request to check if all books has been deleted.
+      this.findall()
+      console.log('Created a customer');
+      console.log(res);
+    });
+  }
+
+  findall() {
+    this.http.get('http://localhost:9090/findall').subscribe((res: Book[]) => {
+      this.AllBooks = res.sort((a, b) => a.id - b.id)
+      console.log('finall books');
+      console.log(res);
+    });
+  }
+
+  get AllBooks() {
+    return this.allBooks
+  }
+
+  set AllBooks(value) {
+    this.allBooks = value
+  }
+
 }
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {FileUploadModule} from 'ng2-file-upload';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, HttpClientModule, FileUploadModule,  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
